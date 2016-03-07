52050306db8e1263523647d6a995113118c65296
Eagle <wei.lee460@gmail.com>
2016-03-07 20:43:10 +0800

    implete a simple table view, only need implement datasource protocol. :-) :-)   .2016-03-07.

--- SimpleTableViewDemo/ViewController.swift
+++ SimpleTableViewDemo/ViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private let dwarves = [
+        "Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dopey",
+        "Thorin", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili",
+        "Oin", "Gloin", "Bifur", "Bofur",
+        "Bombur"
+    ]
+    
+    struct ConstansForTableView {
+        static let SimpleTableIdentifier = "simpleTableIdentifier"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - Table View DataSource Method
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return dwarves.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(ConstansForTableView.SimpleTableIdentifier)
+        if cell == nil
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: ConstansForTableView.SimpleTableIdentifier)
+        }
+        cell!.textLabel?.text = dwarves[indexPath.row]
+        
+        return cell!
     }
 
 
